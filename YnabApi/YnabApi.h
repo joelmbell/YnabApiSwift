9323143c1e97c80822efb11a95479589e093741b
Joel Bell <joel.m.bell@gmail.com>
2018-06-24 21:16:11 -0700

    Initial Commit

---
+++ YnabApi/YnabApi.h
@@ -0,0 +1,19 @@
+//
+//  YnabApi.h
+//  YnabApi
+//
+//  Created by Joel Bell on 6/24/18.
+//  Copyright © 2018 Vivir Creative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YnabApi.
+FOUNDATION_EXPORT double YnabApiVersionNumber;
+
+//! Project version string for YnabApi.
+FOUNDATION_EXPORT const unsigned char YnabApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YnabApi/PublicHeader.h>
+
+
